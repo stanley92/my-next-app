472ad2b9574ded01a0cef0e81c168560ebf8d282
stanley92 <Stanley92@hotmail.com>
2022-05-09 11:05:15 +0800

    Add import statements

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
